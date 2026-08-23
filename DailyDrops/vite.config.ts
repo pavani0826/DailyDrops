592648883f8d34a2b0a42f3d64f826e7be527981
Pavani <Pavani180266@gmail.com>
2026-08-23 18:34:38 +0530

    Updated

--- DailyDrops/vite.config.ts
+++ DailyDrops/vite.config.ts
@@ -27,6 +27,8 @@ export default defineConfig(() => {
         },
         workbox: {
           globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          skipWaiting: true,
+          clientsClaim: true,
         },
       }),
     ],
